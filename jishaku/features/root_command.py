c6cdf7f38989a357e0044688192eb8490ab926b3
Junaid Javed <167234522+hiddeout@users.noreply.github.com>
2024-10-06 14:43:01 -0400

    embed main command

--- jishaku/features/root_command.py
+++ jishaku/features/root_command.py
@@ -46,6 +46,7 @@ class RootCommand(Feature):
 
     @Feature.Command(name="jishaku", aliases=["jsk"],
                      invoke_without_command=True, ignore_extra=False)
+
     async def jsk(self, ctx: ContextA):
         """
         The Jishaku debug and diagnostic commands.
@@ -69,9 +70,9 @@ class RootCommand(Feature):
             dist_version = f'unknown `{discord.__version__}`'
 
         summary = [
-            f"Jishaku v{package_version('jishaku')}, {dist_version}, "
-            f"`Python {sys.version}` on `{sys.platform}`".replace("\n", ""),
-            f"Module was loaded <t:{self.load_time.timestamp():.0f}:R>, "
+            f"Jishaku v{package_version('jishaku')},\n {dist_version},\n "
+            f"`Python,\n {sys.version}` on `{sys.platform}`".replace("\n", ""),
+            f"Module was loaded <t:{self.load_time.timestamp():.0f}:R>,\n "
             f"cog was loaded <t:{self.start_time.timestamp():.0f}:R>.",
             ""
         ]
@@ -106,57 +107,67 @@ class RootCommand(Feature):
                     "to query process information."
                 )
                 summary.append("")  # blank line
-        s_for_guilds = "" if len(self.bot.guilds) == 1 else "s"
-        s_for_users = "" if len(self.bot.users) == 1 else "s"
-        cache_summary = f"{len(self.bot.guilds)} guild{s_for_guilds} and {len(self.bot.users)} user{s_for_users}"
+
+        cache_summary = f"{len(self.bot.guilds)} guild(s) and {len(self.bot.users)} user(s)"
 
         # Show shard settings to summary
         if isinstance(self.bot, discord.AutoShardedClient):
             if len(self.bot.shards) > 20:
                 summary.append(
-                    f"This bot is automatically sharded ({len(self.bot.shards)} shards of {self.bot.shard_count})"
+                    f"> This bot is automatically sharded ({len(self.bot.shards)} shards of {self.bot.shard_count})"
                     f" and can see {cache_summary}."
                 )
             else:
                 shard_ids = ', '.join(str(i) for i in self.bot.shards.keys())
                 summary.append(
-                    f"This bot is automatically sharded (Shards {shard_ids} of {self.bot.shard_count})"
+                    f"> This bot is automatically sharded (Shards {shard_ids} of {self.bot.shard_count})"
                     f" and can see {cache_summary}."
                 )
         elif self.bot.shard_count:
             summary.append(
-                f"This bot is manually sharded (Shard {self.bot.shard_id} of {self.bot.shard_count})"
+                f"> This bot is manually sharded (Shard {self.bot.shard_id} of {self.bot.shard_count})"
                 f" and can see {cache_summary}."
             )
         else:
-            summary.append(f"This bot is not sharded and can see {cache_summary}.")
+            summary.append(f"> This bot is not sharded and can see {cache_summary}.")
 
         # pylint: disable=protected-access
         if self.bot._connection.max_messages:  # type: ignore
-            message_cache = f"Message cache capped at {self.bot._connection.max_messages}"  # type: ignore
+            message_cache = f"> Message cache capped at {self.bot._connection.max_messages}"  # type: ignore
         else:
             message_cache = "Message cache is disabled"
 
-        remarks = {
-            True: 'enabled',
-            False: 'disabled',
-            None: 'unknown'
-        }
+        if discord.version_info >= (1, 5, 0):
+            remarks = {
+                True: 'enabled',
+                False: 'disabled',
+                None: 'unknown'
+            }
 
-        *group, last = (
-            f"{intent.replace('_', ' ')} intent is {remarks.get(getattr(self.bot.intents, intent, None))}"
-            for intent in
-            ('presences', 'members', 'message_content')
-        )
+            *group, last = (
+                f"{intent.replace('_', ' ')} intent is {remarks.get(getattr(self.bot.intents, intent, None))}"
+                for intent in
+                ('presences', 'members', 'message_content')
+            )
 
-        summary.append(f"{message_cache}, {', '.join(group)}, and {last}.")
+            summary.append(f"{message_cache}, {', '.join(group)}, and {last}.")
+        else:
+            guild_subscriptions = f"> guild subscriptions are {'enabled' if self.bot._connection.guild_subscriptions else 'disabled'}"  # type: ignore
+
+            summary.append(f"{message_cache} and {guild_subscriptions}.")
 
         # pylint: enable=protected-access
 
         # Show websocket latency in milliseconds
-        summary.append(f"Average websocket latency: {round(self.bot.latency * 1000, 2)}ms")
+        summary.append(f"> **Average websocket latency:** {round(self.bot.latency * 1000, 2)}ms")
 
-        await ctx.send("\n".join(summary))
+        # Create an embed with the summary
+        embed = discord.Embed(
+            description="\n".join(summary),
+            color=discord.Color.dark_embed()
+        )
+
+        await ctx.send(embed=embed)
 
     # pylint: disable=no-member
     @Feature.Command(parent="jsk", name="hide")
